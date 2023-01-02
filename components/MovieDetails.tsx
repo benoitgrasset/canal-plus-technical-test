89cb70ebc058e91e17cf0479b25adf97460b46c8
Benoit GRASSET <32497923+benoitgrasset@users.noreply.github.com>
2023-01-02 15:43:50 +0100

    fix overflow

--- components/MovieDetails.tsx
+++ components/MovieDetails.tsx
@@ -35,77 +35,75 @@ const MovieDetails: FC<Props> = ({ result }) => {
   const classes = useStyles({ backdropPath });
 
   return (
-    <main className={classes.main}>
-      <div className={classes.movieDetails}>
-        <div className={classes.movieBackground}>
-          <div className={classes.customBg}>
-            <div className={classes.flexContainer}>
-              <div className={clsx(classes.imgWrapper, classes.flex1)}>
-                <Image
-                  quality={75}
-                  src={posterPath}
-                  alt="backdrop"
-                  loading="lazy"
-                  placeholder="blur"
-                  blurDataURL={rgbDataURL(159, 165, 164)}
-                  className={classes.image}
-                  width={width}
-                  height={(width * 3) / 2}
-                />
+    <div className={classes.movieDetails}>
+      <div className={classes.movieBackground}>
+        <div className={classes.customBg}>
+          <div className={classes.flexContainer}>
+            <div className={clsx(classes.imgWrapper, classes.flex1)}>
+              <Image
+                quality={75}
+                src={posterPath}
+                alt="backdrop"
+                loading="lazy"
+                placeholder="blur"
+                blurDataURL={rgbDataURL(159, 165, 164)}
+                className={classes.image}
+                width={width}
+                height={(width * 3) / 2}
+              />
+            </div>
+            <div className={classes.flex2}>
+              <h1 className={classes.title}>{title}</h1>
+              <div className={classes.section}>
+                <div>
+                  <h3>Average votes</h3>
+                  <CircularProgressWithLabel
+                    value={vote_average * 10}
+                    label={vote_average}
+                  />
+                </div>
+                <div>
+                  <h3>Popularity</h3>
+                  <div>{Math.round(popularity)}</div>
+                </div>
               </div>
-              <div className={classes.flex2}>
-                <h1 className={classes.title}>{title}</h1>
-                <div className={classes.section}>
-                  <div>
-                    <h3>Average votes</h3>
-                    <CircularProgressWithLabel
-                      value={vote_average * 10}
-                      label={vote_average}
-                    />
-                  </div>
-                  <div>
-                    <h3>Popularity</h3>
-                    <div>{Math.round(popularity)}</div>
-                  </div>
-                </div>
-                <div className={classes.section}>
-                  <div>
-                    <h3>Released</h3>
-                    <div>{release_date}</div>
-                  </div>
-                  <div>
-                    <h3>Duration</h3>
-                    <div>{Math.round(runtime / 60) + ' h'}</div>
-                  </div>
-                  <div>
-                    <h3>Language</h3>
-                    <div>{original_language}</div>
-                  </div>
+              <div className={classes.section}>
+                <div>
+                  <h3>Released</h3>
+                  <div>{release_date}</div>
                 </div>
                 <div>
-                  <h3>Number of votes</h3>
-                  <div>{vote_count}</div>
+                  <h3>Duration</h3>
+                  <div>{Math.round(runtime / 60) + ' h'}</div>
                 </div>
-                {genres && (
-                  <div>
-                    <h3>Genres</h3>
-                    <div className={classes.genres}>
-                      {genres.map((genre) => (
-                        <span key={genre.id}>{genre.name}</span>
-                      ))}
-                    </div>
-                  </div>
-                )}
                 <div>
-                  <h3>Synopsis</h3>
-                  <p>{overview}</p>
+                  <h3>Language</h3>
+                  <div>{original_language}</div>
                 </div>
+              </div>
+              <div>
+                <h3>Number of votes</h3>
+                <div>{vote_count}</div>
+              </div>
+              {genres && (
+                <div>
+                  <h3>Genres</h3>
+                  <div className={classes.genres}>
+                    {genres.map((genre) => (
+                      <span key={genre.id}>{genre.name}</span>
+                    ))}
+                  </div>
+                </div>
+              )}
+              <div>
+                <h3>Synopsis</h3>
+                <p>{overview}</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
